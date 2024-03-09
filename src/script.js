1143251b8299456bf57f6248e550a4916b440202
10Dozen <steel.mjr@gmail.com>
2024-03-09 11:43:28 +0300

    Added script-based URLs, added captions, fixed broken discord url, added animated preset tutorial

---
+++ src/script.js
@@ -0,0 +1,34 @@
+
+const CommunityURLs = [
+	{
+		name: "discord",
+		url: "https://discord.gg/bqnGUCmAgU"
+	},
+	{
+		name: "teamspeak",
+		url: "ts3server://ts3.tacticalshift.ru?port=9987"
+	},
+	{
+		name: "vk",
+		url: "https://vk.com/tacticalshift"
+	},
+	{
+		name: "steam",
+		url: "https://steamcommunity.com/groups/tacticalshift"
+	},
+	{
+		name: "youtube",
+		url: "https://www.youtube.com/channel/UCUCo9u8-z0pLinUEWzSlPmg"
+	}
+]
+
+
+function setCommunityURLs() {
+	CommunityURLs.forEach((url) => {
+		document
+			.querySelectorAll(`[meta-url=${url.name}`)
+			.forEach((link) => link.href = url.url)	
+	})
+};
+
+document.addEventListener("DOMContentLoaded", setCommunityURLs);
